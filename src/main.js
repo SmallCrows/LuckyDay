156fcc4faf0a06c044cb1eb5f267af0b824d85c8
SmallCrows <16991741@qq.com>
2026-04-14 12:47:26 +0800

    增加启动空白显示默认内容

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import { mount } from 'svelte';
+import './app.css';
+import App from './App.svelte';
 
-// 插件会自动注入虚幻模块来实现注册
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register';
+registerSW({ immediate: true });
+
+const targetNode = document.getElementById('app');
+
+// 1. 清空过渡骨架屏
+targetNode.innerHTML = '';
 
 const app = mount(App, {
-  target: document.getElementById('app'),
-})
+  target: targetNode,
+});
 
-export default app
+export default app;
